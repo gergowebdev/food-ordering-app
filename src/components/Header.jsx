83d8492dc501659658034aa0c75a01abe1a4d720
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-08-14 14:16:57 +0200

    Adding a Reusable Modal component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,19 @@ import { useContext } from "react";
 import CartContext from "../store/CartContext";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return (
         <header id="main-header">
@@ -16,7 +23,9 @@ export default function Header() {
                 <h1>Gergo's Food</h1>
             </div>
             <nav>
-                <Button textOnly={true}>Cart ({totalCartItems})</Button>
+                <Button textOnly={true} onClick={handleShowCart}>
+                    Cart ({totalCartItems})
+                </Button>
             </nav>
         </header>
     );
